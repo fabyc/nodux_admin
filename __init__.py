b3cda29b5d83b933a908a41e61870e268116c5a4
Tatiana Quizhpe <tatianaq@nodux.ec>
2017-03-03 12:08:28 -0500

    Add check in permission

--- __init__.py
+++ __init__.py
@@ -2,8 +2,12 @@
 #this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
 from .user import *
+from .company import *
+from .party import *
 
 def register():
     Pool.register(
         User,
+        Company,
+        Party,
         module='nodux_admin', type_='model')
